816340aa94e6875d8267cc15474399067969eb4d
Walaa shabaan <walaa.ai.shaaban@gmail.com>
2024-08-14 22:57:25 +0300

    add mvc and dotenv

--- src/routes/base.py
+++ src/routes/base.py
@@ -4,16 +4,10 @@ import os
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
-
 )
 
 @base_router.get("/")
 async def welcome():
-    app_name = os.getenv('APP_NAME')
-    version = os.getenv('APP_VERSION')
-
-    return {
-        'app_name' : app_name,
-        'version' : version
-
-    }
+        app_name = os.getenv("APP_NAME")
+        app_version = os.getenv("APP_VERSION")
+        return {"app_name": app_name, "app_version": app_version}
